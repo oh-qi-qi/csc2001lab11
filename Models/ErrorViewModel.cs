762930920348721be6ac2f9340474306240ca7c5
Oh Qi Qi <ohqiiqii@gmail.com>
2020-11-19 12:31:10 +0800

    dotnet sdk 3.1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace csc2001lab11.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
